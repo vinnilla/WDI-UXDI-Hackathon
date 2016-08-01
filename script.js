192843a6901c9fdbc3447c21c0a077c014176b35
Amanda Beaudin <amandalbeaudin@gmail.com>
2016-08-01 13:37:21 -0700

    adjusted verbs to past tense

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ console.log('script is running')
 var nouns = ["couch", "brother", "door", "gave", "good", "cat", "could", "girl", "boy", "sister", "prince", "queen", "lion", "mouse", "turtle", "store", "beach", "house", "pillow", "watch"]
 var adjectives =["brave", "bright", "calm", "strong", "nice", "honest", "grateful", "proud", "wild", "wise", "excited", "slim", "fair", "great", "smart", "fresh", "new", "blue", "green", "fast"]
 var prepositions = ["about", "through", "over", "into", "with", "plus", "below", "like", "before", "until", "after", "except", "save", "around", "since", "toward", "without", "behind", "up", "out"]
-var verbs = ["dance", "laugh", "bounce", "shout", "bite", "jump", "swim", "kick", "eat", "dig", "walk", "stir", "run", "chop", "gave", "make", "sleep", "hug", "hide", "sing"]
+var verbs = ["danced", "laughed", "bounced", "shouted", "bit", "jumped", "swam", "kicked", "ate", "skipped", "walked", "stired", "ran", "choped", "gave", "made", "slept", "huged", "hid", "sang"]
 var charNames = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu"]
 
