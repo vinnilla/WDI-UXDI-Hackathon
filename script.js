e9d4b3c811928ed331ed4aa7562b1c6e58544235
Vincent <vincentckhuang@gmail.com>
2016-08-02 20:04:49 -0700

    replaced png with gifs

--- script.js
+++ script.js
@@ -13,30 +13,38 @@ var adjectives =["brave", "bright", "calm", "strong", "nice", "honest", "gratefu
 var prepositions = ["with"]
 var verbs = ["danced", "laughed", "bounced", "shouted", "jumped", "swam", "skipped", "walked", "ran", "jogged", "cried", "complained", "spoke", "traveled", "read", "fought", "wrestled", "wrote", "dreamt", "played", "shared"]
 var brokenVerbs = ["d_nced", "la_ghed", "b_unced", "sho_ted", "j_mped", "sw_m", "sk_pped", "w_lked", "r_n", "j_gged", "cr_ed", "c_mplained", "sp_ke", "tr_veled", "r_ad", "f_ught", "wr_stled", "wr_te", "dr_amt", "pl_yed", "sh_red"]
-var charNames = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Caterpie", "Vulpix", "Jigglypuff", "Diglett", "Psyduck", "Growlithe", "Abra", "Machop", "Ponyta", "Slowpoke", "Seel", "Gastly", "Drowzee", "Cubone", "Scyther", "Ditto", "Eevee", "Mewtwo", "Mew"]
-var charImages = ["url('http://media.pldh.net/pokemon/gen5/blackwhite/001.png')",
-	"url('http://media.pldh.net/pokemon/gen5/blackwhite/004.png')",
-	"url('http://media.pldh.net/pokemon/gen4/dp/007.png')",
-	"url('http://media.pldh.net/pokemon/gen4/dp/025.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/caterpie.png')",
-	"url('https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/vulpix.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/jigglypuff.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/diglett.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/psyduck.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/growlithe.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/abra.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/machop.png')",
-	"url('https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/ponyta.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/slowpoke.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/seel.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/gastly.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/drowzee.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/cubone.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/scyther-f.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/ditto.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/eevee.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/mewtwo.png')",
-	"url('https://img.pokemondb.net/sprites/black-white/normal/mew.png')"]
+var charNames = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Pikachu", "Raichu", "Caterpie", "Vulpix", "Jigglypuff", "Diglett", "Psyduck", "Growlithe", "Abra", "Machop", "Ponyta", "Slowpoke", "Seel", "Gastly", "Drowzee", "Cubone", "Scyther", "Ditto", "Eevee", "Mewtwo", "Mew"]
+var charImages = 
+	["url('https://img.pokemondb.net/sprites/black-white/anim/normal/bulbasaur.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/ivysaur.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/charmander.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/charmeleon.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/squirtle.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/wartortle.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/raichu.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/caterpie.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/vulpix.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/jigglypuff.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/diglett.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/psyduck.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/growlithe.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/abra.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/machop.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/ponyta.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/slowpoke.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/seel.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/gastly.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/drowzee.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/cubone.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/scyther-f.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/ditto.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/eevee.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/mewtwo.gif')",
+	"url('https://img.pokemondb.net/sprites/black-white/anim/normal/mew.gif')"]
 var vowel = '';
 var word = '';
 var check = '';
